45b799cbc57054a2c74588014f67cdf3251a7c1c
Luisa Carmona <cp.carmonaclf@gmail.com>
2021-11-16 01:12:22 -0500

    primer compromiso

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  //res.render('index', { title: 'Hola mundo Maravilloso!' });
+  res.send("Prueba sin motor de visual")
+});
+
+module.exports = router;
